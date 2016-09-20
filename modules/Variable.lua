46c6d86a5df5ad187d02dc0488646a31bd10a370
VariableV <thehacklor@gmail.com>
2016-09-19 20:22:17 -0500

    Update Variable.lua

--- modules/Variable.lua
+++ modules/Variable.lua
@@ -5,5 +5,15 @@
   
 return
 {
+  -- useles but yolo
+  function insertasset(type, id, player)
+    local insertService = game:service'InsertService'
+    if type == 'hat' then
+      insertService:LoadAsset(id):children''[1].Parent = parent.Character
+    elseif type == 'gear' then
+      insertService:LoadAsset(id):children''[1].Parent = parent.Backpack
+    else
+      warn('invalid type: '..type)
+  end
   
 }
