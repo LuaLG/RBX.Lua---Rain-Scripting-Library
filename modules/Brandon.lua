9a950a56aff20d3324c6fcb85406db647d53b04f
L0CATE <b-tr110@hotmail.com>
2016-09-22 18:00:27 -0500

    Update Brandon.lua

--- modules/Brandon.lua
+++ modules/Brandon.lua
@@ -1,4 +1,29 @@
---Brandon's magical functions
---TODO: magic()
+-- Brandon's magical functions
 
-return {}
+local lib = {}
+
+-- Similar to table.foreach, but for instances
+-- Function `f` receives only the child instance as a parameter
+lib.foreach_child = function(inst, f)
+	local success, children = pcall(inst.GetChildren, inst)
+	if success == false then return end	
+	for i,v in next, inst:GetChildren() do
+		if f(v) ~= nil then
+			return
+		end
+	end
+end
+
+-- Same as foreach_child, except it recursively iterates through all descendants
+lib.foreach_descendant = function(inst, f)
+	local success, children = pcall(inst.GetChildren, inst)
+	if success == false then return end
+	for i,v in next, children do
+		if f(v) ~= nil then
+			return
+		end
+		lib.foreach_descendant(v, f)
+	end
+end
+
+return lib
