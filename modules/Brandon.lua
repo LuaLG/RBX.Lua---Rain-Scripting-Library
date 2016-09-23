9ad9996a445ea37813babe5a66794064f86af8be
L0CATE <b-tr110@hotmail.com>
2016-09-22 19:55:47 -0500

    Update Brandon.lua
    Add magick

--- modules/Brandon.lua
+++ modules/Brandon.lua
@@ -7,7 +7,7 @@ local lib = {}
 lib.foreach_child = function(inst, f)
 	local success, children = pcall(inst.GetChildren, inst)
 	if success == false then return end	
-	for i,v in next, children do
+	for i,v in next, inst:GetChildren() do
 		if f(v) ~= nil then
 			return
 		end
@@ -26,4 +26,19 @@ lib.foreach_descendant = function(inst, f)
 	end
 end
 
-return lib
+-- Overthrow Autumn and seize the means of production!
+lib.seize_rain = function()
+	local autumn_tbl = require(script.Parent.Autumn)
+	for i,v in next, autumn_tbl do autumn_tbl[i] = nil end
+	setmetatable(autumn_tbl, {
+		__index = function(t,k) return function() print("Rain has been seized!") end end,
+		__newindex = function(t,k,v) return end,
+		__metatable = "Rain has been seized!"
+	})
+end
+
+return setmetatable({}, {
+	__index = function(_,k) return lib[k] end,
+	__newindex = function() return end,
+	__metatable = "The metatable is locked"
+})
